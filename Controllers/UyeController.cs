33309357a197aa963ed5f7a82c52df1368d72e39
Recai cingöz <recaicingz@gmail.com>
2016-05-03 02:52:51 +0300

    Added files via upload

---
+++ Controllers/UyeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Uyegirisuygulama.Models;
+
+namespace Uyegirisuygulama.Controllers
+{
+    public class UyeController : Controller
+    {
+        //
+        // GET: /Uye/
+        [HttpGet]
+        public ActionResult Giris()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Giris(Kullanicilar Kul)
+        {
+            if (ModelState.IsValid)
+            {
+                using (UyelikEntities DB = new UyelikEntities())
+                {
+                    var Uyegirisi = DB.Kullanicilar.Where(U => U.Kul_AD.Equals(U.Kul_AD) && U.Kul_PAROLA.Equals(U.Kul_PAROLA)).FirstOrDefault();
+                    if (Uyegirisi != null)
+                    {
+                        Session["GirisKullaniciID"] = Uyegirisi.Kul_ID.ToString();
+                        Session["GirisKullaniciAD"] = Uyegirisi.Kul_AD.ToString();
+                        return RedirectToAction("Profil");
+                    }
+                }
+                
+            }
+            return View(Kul);
+        }
+
+        public ActionResult Profil()
+        {
+            if (Session["GirisKullaniciID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+
+	}
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Uyegirisuygulama.MvcApplication" Language="C#" %>
